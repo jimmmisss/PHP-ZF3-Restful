f78e4a13e48ad58205a637f75db2e4524887bdde
Wesley F Pereira <jimmmisss@gmail.com>
2018-01-24 16:09:45 -0300

    Projeto em andamento, desenvolvendo uma api restful com zend framework 3

--- public/js/bootstrap.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.navbar-xbootstrap').click(function(){
-        $('.nav-xbootstrap').toggleClass('visible');
-        $('body').toggleClass('cover-bg');
-    })
-});
